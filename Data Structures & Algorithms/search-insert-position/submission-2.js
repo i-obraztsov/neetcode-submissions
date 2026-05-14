87742aab743c2bade097b13cda0bb96d5a38660b
Ivan Obraztsov <o6raztsv@gmail.com>
2026-05-14 13:00:49 +0200

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums, target) {
+        let l = 0;
+        let r = nums.length;
+
+        while (l < r) {
+            let m = l + Math.floor((r - l) / 2);
+            if (nums[m] >= target) {
+                r = m;
+            } else {
+                l = m + 1
+            }
+        }
+
+        return l;
+    }
+}
